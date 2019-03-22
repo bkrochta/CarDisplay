f7d01119d7bcc359f1525dff74a20334bea42c43
Ben <bkrochta9@gmail.com>
2019-03-22 14:42:58 -0400

    mpu9250

--- mpu9250.py
+++ mpu9250.py
@@ -221,6 +221,17 @@ class MPU9250:
 
         return {"x":x, "y":y, "z":z}
 
+    ## Data Convert
+    # @param [in] self The object pointer.
+    # @param [in] data1 LSB
+    # @param [in] data2 MSB
+    # @retval Value MSB+LSB(int 16bit)
+    def dataConv(self, data1, data2):
+        value = data1 | (data2 << 8)
+        if(value & (1 << 16 - 1)):
+            value -= (1<<16)
+        return value
+
 test = MPU9250()
 while True:
     print("Accel ", test.readAccel())
