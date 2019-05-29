9c03f455b1f1d50ee2839eeda246083750a350cc
Ben <bkrochta9@gmail.com>
2019-05-28 20:57:17 -0400

    Beta GUI Done

--- view.py
+++ view.py
@@ -4,17 +4,20 @@ import thermometer
 import time
 import tkinter
 import threading
+import datetime
+import math
 
 # Thermometer addresses
 IN_THERM_ADDR = '28-031597799b7d'
 OUT_THERM_ADDR = '28-0315977942f6'
 
 # Globals
-ct = 0
-in_temp = 0
-out_temp = 0
-direction = 0
-speed = 0
+in_temp = '--'
+out_temp = '--'
+direction = '--'
+speed = '--'
+avg_speed = 20
+dst_traveled = 500
 quit = 0
 scale = 4.5
 
@@ -24,6 +27,8 @@ def __in_temp_thread():
     in_therm = thermometer.Thermometer(IN_THERM_ADDR)
     while quit == 0:
         in_temp = in_therm.get_temp()
+        if in_temp == None:
+            in_temp = 'er'
 
 
 def __out_temp_thread():
@@ -32,15 +37,24 @@ def __out_temp_thread():
     out_therm = thermometer.Thermometer(OUT_THERM_ADDR)
     while quit == 0:
         out_temp = out_therm.get_temp()
+        if out_temp == None:
+            out_temp = 'er'
 
 
 def __gps_thread():
     global speed
+    global avg_speed
+    global dst_traveled
 
     g = gps.GPS()
+    g.update()
+    g.update_time()
     while quit == 0:
         g.update()
-        speed = g.get_speed()
+        speed = math.floor(g.get_speed())
+        # avg_speed = int(g.get_average_speed())
+        # dst_traveled = int(g.get_distance_traveled())
+        time.sleep(0.200)
 
 
 def __mpu_thread():
@@ -50,55 +64,79 @@ def __mpu_thread():
     #mpu.calibrate()
     while quit == 0:
         direction = mpu.get_heading()
+        time.sleep(0.200)
 
-def __quit():
+def __quit(e):
+    global quit
+
     quit = 1
-    root.detroy()
+    root.destroy()
 
 
 root = tkinter.Tk()
 root.attributes("-fullscreen", True)
 root.configure(background='black')
+root.bind("<Escape>", __quit)
 
-clock_label = tkinter.Label(root, font=('arial',100/scale, 'bold'), fg='red', bg='black')
-dir_label = tkinter.Label(root, font=('arial',100/scale, 'bold'), fg='red', bg='black')
-temp_in_label = tkinter.Label(root, font=('arial',100/scale, 'bold'), fg='red', bg='black')
-temp_out_label = tkinter.Label(root, font=('arial',100/scale, 'bold'), fg='red', bg='black')
-speed_label = tkinter.Label(root, font=('arial',400/scale, 'bold'), fg='red', bg='black')
+# Create data labels
+clock_label = tkinter.Label(root, font=('arial',int(300/scale), 'bold'), fg='red', bg='black')
+dir_label = tkinter.Label(root, font=('arial',int(200/scale), 'bold'), fg='red', bg='black')
+temp_in_label = tkinter.Label(root, font=('arial',int(200/scale), 'bold'), fg='red', bg='black')
+temp_out_label = tkinter.Label(root, font=('arial',int(200/scale), 'bold'), fg='red', bg='black')
+speed_label = tkinter.Label(root, font=('arial',int(800/scale), 'bold'), fg='red', bg='black')
+# avg_speed_label = tkinter.Label(root, font=('arial',int(200/scale), 'bold'), fg='red', bg='black')
+# dst_traveled_label = tkinter.Label(root, font=('arial',int(200/scale), 'bold'), fg='red', bg='black')
 
+# Create unit and description labels
+in_label = tkinter.Label(root, font=('arial',int(100/scale), 'bold'), fg='red', bg='black', text='in:')
+# mph_label1 = tkinter.Label(root, font=('arial',int(100/scale), 'bold'), fg='red', bg='black', text='mph')
+# mph_label2 = tkinter.Label(root, font=('arial',int(50/scale), 'bold'), fg='red', bg='black', text='mph')
+# miles_label = tkinter.Label(root, font=('arial',int(50/scale), 'bold'), fg='red', bg='black', text='miles')
+# as_label = tkinter.Label(root, font=('arial',int(100/scale), 'bold'), fg='red', bg='black', text='avg speed:')
+# dt_label = tkinter.Label(root, font=('arial',int(100/scale), 'bold'), fg='red', bg='black', text='dst traveled:')
+
+# Place data labels
 clock_label.place(relx=0.2, rely=0, relheight=0.2, relwidth=0.6)
 dir_label.place(relx=0, rely=0, relheight=0.2, relwidth=0.2)
 temp_in_label.place(relx=0, rely=0.8, relheight=0.2, relwidth=0.2)
 temp_out_label.place(relx=0.8, rely=0, relheight=0.2, relwidth=0.2)
-speed_label.place(relx=0.3, rely=0.3, relheight=0.6, relwidth=0.6)
+speed_label.place(relx=0.2, rely=0.2, relheight=0.6, relwidth=0.6)
+# avg_speed_label.place(relx=0.2, rely=0.8, relheight=0.2, relwidth=0.2)
+# dst_traveled_label.place(relx=0.4, rely=0.8, relheight=0.2, relwidth=0.2)
 
-tkinter.Button(root, text="Quit", command=__quit).place(relx=0.8, rely=0.5, relheight=0.2, relwidth=0.2)
+# Place unit and description labels
+in_label.place(relx=0.0, rely=0.775, relheight=0.05, relwidth=0.075)
+# mph_label1.place(relx=0.8, rely=0.7, relheight=0.05, relwidth=0.075)
+# mph_label2.place(relx=0.4, rely=0.9, relheight=0.05, relwidth=0.075)
+# miles_label.place(relx=0.6, rely=0.9, relheight=0.05, relwidth=0.075)
+# as_label.place(relx=0.2, rely=0.775, relheight=0.05, relwidth=0.075)
+# dt_label.place(relx=0.4, rely=0.775, relheight=0.05, relwidth=0.075)
 
-in_therm_thread = threading.Thread(task=__in_temp_thread)
-out_therm_thread = threading.Thread(task=__out_temp_thread)
-mpu_thread = threading.Thread(task=__mpu_thread)
-gps_thread = threading.Thread(task=__gps_thread)
+# Debugging quit button
+#tkinter.Button(root, text="Quit", command=lambda: __quit(None)).place(relx=0.8, rely=0.5, relheight=0.2, relwidth=0.2)
 
+# create threads for sensors
+in_therm_thread = threading.Thread(target=__in_temp_thread)
+out_therm_thread = threading.Thread(target=__out_temp_thread)
+mpu_thread = threading.Thread(target=__mpu_thread)
+gps_thread = threading.Thread(target=__gps_thread)
+
+# start threads
 in_therm_thread.start()
-# out_therm_thread.start()
+out_therm_thread.start()
 mpu_thread.start()
 gps_thread.start()
 
-
-
-
-def tick():
-    global ct
-
+# MAIN LOOP #
+while True:
     dir_label.config(text=direction)
-    clock_label.config(text=clock)
-    temp_in_label.config(text=in__therm)
-    temp_out_label.config(text=ct)
-    ct += 1
-    # calls itself every 200 milliseconds
-    # to update the time display as needed
-    # could use &gt;200 ms, but display gets jerky
-    clock.after(200, tick)
+    clock_label.config(text=datetime.datetime.now().strftime('%-I:%M'))
+    temp_in_label.config(text=str(in_temp) + '\u00b0F')
+    temp_out_label.config(text=str(out_temp) + '\u00b0F')
+    speed_label.config(text=speed)
+    # avg_speed_label.config(text=avg_speed)
+    # dst_traveled_label.config(text=dst_traveled)
 
-tick()
-root.mainloop()
+    root.update_idletasks()
+    root.update()
+    time.sleep(0.200)
